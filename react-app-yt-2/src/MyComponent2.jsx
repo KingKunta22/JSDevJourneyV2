50223c94459d2db4b3dda1b83ff77db016e5bf67
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-05-17 09:53:09 +0800

    Adding qty and textarea

--- react-app-yt-2/src/MyComponent2.jsx
+++ react-app-yt-2/src/MyComponent2.jsx
@@ -1,18 +1,38 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 function MyComponent2() {
+  const [name, setName] = useState("Guest");
+  const [qty, setQty] = useState(1);
+  const [comment, setComment] = useState("");
 
-    const [name, setName] = useState("Guest")
-    function handleNameChange(event) {
-        setName(event.target.value);
-    }
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
 
-    return (
-        <>
-            <input type="text" value={name} onChange={handleNameChange}/>
-            <p>Name: {name}</p>
-        </>
-    )
+  function handleQtyChange(event) {
+    setQty(event.target.value);
+  }
+
+  function handleCommentChange(event) {
+    setComment(event.target.value);
+  }
+
+  return (
+    <>
+      <input type="text" value={name} onChange={handleNameChange} />
+      <p>Name: {name}</p>
+
+      <input type="number" value={qty} onChange={handleQtyChange} />
+      <p>Qty: {qty}</p>
+
+      <textarea
+        value={comment}
+        onChange={handleCommentChange}
+        placeholder="Enter your comment here"
+      />
+      <p>Comment: {comment}</p>
+    </>
+  );
 }
 
-export default MyComponent2
+export default MyComponent2;
