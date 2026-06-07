7e07c342496a13ffe9a633094c97a3855ba3528d
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-06-07 13:20:22 +0800

    Starting overrrr

---
+++ Day-45ish/src/App.jsx
@@ -0,0 +1,7 @@
+import Refresher from "./Refresher.jsx";
+
+function App() {
+  return <Refresher />;
+}
+
+export default App;
---
+++ Day-45ish/src/Refresher.jsx
@@ -0,0 +1,5 @@
+function Refresher() {
+  return <h1>Hello?</h1>;
+}
+
+export default Refresher;
