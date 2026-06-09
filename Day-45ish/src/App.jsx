36a5bdeb56a5a4d4eeaba6a4e80e8d0b2307ce0f
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-06-09 08:18:40 +0800

    ChatGPT4

--- Day-45ish/src/App.jsx
+++ Day-45ish/src/App.jsx
@@ -1,10 +1,11 @@
 // import Refresher from "./Refresher.jsx";
 // import Counter from "./Counter.jsx" // Day 45
 // import TodoList from "./TodoList.jsx" // Day 46
-import ControlledInputs from "./ControlledInputs.jsx"; // Day 47
+// import ControlledInputs from "./Days/ControlledInputs.jsx"; // Day 47
+import ControlledInputs2 from "./Days/ControlledInputs2.jsx"; // Day 47
 
 function App() {
-  return <ControlledInputs />;
+  return <ControlledInputs2 />;
 }
 
 export default App;
---
+++ Day-45ish/src/Days/ControlledInputs2.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+function ControlledInputs2() {
+
+    const [input, setInput] = useState("")
+
+    return(
+        <>
+        <div>
+            <input type="text" value= />
+        </div>
+        </>
+    )
+}
+
+export default ControlledInputs2
