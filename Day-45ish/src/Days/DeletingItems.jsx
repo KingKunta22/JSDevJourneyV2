ee34bb0ab787f82ad55e811fa4ac8098e8447365
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-06-11 08:05:18 +0800

    Lost

--- Day-45ish/src/Days/DeletingItems.jsx
+++ Day-45ish/src/Days/DeletingItems.jsx
@@ -9,7 +9,37 @@ function DeletingItems() {
     setNote("");
   }
 
-  function handleDeleteNote(index) {}
+  //   function handleDeleteNote(idx) {
+  //     // Hmm, how do I get all the li's here....
+  //     notes.filter((note, index) => {
+  //         if(index !== idx) {
+  //             return <li>{note}</li>
+  //         }
+  //     })
+  //     // Damn, I wonder why it's not orkinggg.........
+  //   }
+
+  //   function handleDeleteNote(idx) {
+  //     notes.filter((note, index) => {
+  //         console.log(idx)
+  //         console.log(index)
+  //     })
+  //     console.log("Hello")
+  //   }
+  // But it's working here, so it should be the return or no?????? what could be the problem, should be the syntax rightt
+  // Man, I was able to do this last time but I can't get it right now, that should mean I don't understand it fully yet hayssssssssssssssssssss
+
+  function handleDeleteNote(idx) {
+    notes.filter((note, index) => {
+    //   console.log(idx);
+    //   console.log(index);
+    //   if(index !== idx) {
+        
+    //   }
+    return index !== idx
+    });
+    console.log("Hello");
+  }
 
   return (
     <>
@@ -32,7 +62,9 @@ function DeletingItems() {
         <h2>Notes Added</h2>
         <ul>
           {notes.map((note, index) => (
-            <li key={index}>{note}</li>
+            <li key={index}>
+              {note} <button onClick={(e) => handleDeleteNote(index)}>X</button>
+            </li>
           ))}
         </ul>
       </div>
