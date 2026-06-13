71117007a466014c640799026a53ee18ac4e7c76
Leonard Chloie Sagarino <kulasagarino123@gmail.com>
2026-06-13 14:22:23 +0800

    Finished handleAdd and onChange

--- Day-45ish/src/Days/UpdatingItems3.jsx
+++ Day-45ish/src/Days/UpdatingItems3.jsx
@@ -1,12 +1,35 @@
 // Matter of fact, I'm restarting everything cus i been stuck on this part for too long
 // Yk, just to refresh the ideas that I had...
 
-import { useState } from 'react'
+import { useState } from 'react';
 
 function UpdatingItems3() {
+    // So we need two states here, one for the input, and one to store the state or the saved input
+    const [note, setNote] = useState("");
+    const [notes, setNotes] = useState([])
+
+    function handleAddNote(){
+        setNotes(note)
+        setNote("")
+    }
+
     return(
         <>
         <h1>Todo List App (Vibe-Code Free)</h1>
+        <div>
+            <input type="text" placeholder="Enter a task" value={note} onChange={setNote(note)}/>
+            <button onClick={handleAddNote}>Add</button>
+        </div>
+        <br />
+        <p>{note}</p>
+        <br />
+        <div>
+            <ol>
+                
+            </ol>
+        </div>
         </>
     )
 }
+
+export default UpdatingItems3;
